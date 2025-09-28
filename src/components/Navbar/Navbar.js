360038953633f9185786907813a8956097fe987a
Lochinbek Dehkonov <103257271+lochinbek9@users.noreply.github.com>
2025-09-28 09:20:03 +0500

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Navbar.scss";
-import {Link} from "react-router-dom";
-import { useSelector, useDispatch} from 'react-redux';
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
 import { setSidebarOn } from '../../store/sidebarSlice';
 import { getAllCategories } from '../../store/categorySlice';
 import { getAllCarts, getCartItemsCount, getCartTotal } from '../../store/cartSlice';
@@ -10,28 +10,27 @@ import CartModal from "../CartModal/CartModal";
 const Navbar = () => {
   const dispatch = useDispatch();
   const categories = useSelector(getAllCategories);
-  console.log(categories);
   const carts = useSelector(getAllCarts);
   const itemsCount = useSelector(getCartItemsCount);
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearchTerm = (e) => {
-    e.preventDefault();
     setSearchTerm(e.target.value);
-  }
+  };
 
   useEffect(() => {
     dispatch(getCartTotal());
-  }, [carts])
+  }, [carts, dispatch]);
 
   return (
     <nav className='navbar'>
       <div className='navbar-cnt flex align-center'>
+        {/* Sidebar toggle and brand */}
         <div className='brand-and-toggler flex align-center'>
-          <button type = "button" className='sidebar-show-btn text-white' onClick={() => dispatch(setSidebarOn())}>
+          <button type="button" className='sidebar-show-btn text-white' onClick={() => dispatch(setSidebarOn())}>
             <i className='fas fa-bars'></i>
           </button>
-          <Link to = "/" className='navbar-brand flex align-center'>
+          <Link to="/" className='navbar-brand flex align-center'>
             <span className='navbar-brand-ico'>
               <i className='fa-solid fa-bag-shopping'></i>
             </span>
@@ -44,40 +43,44 @@ const Navbar = () => {
         <div className='navbar-collapse w-100'>
           <div className='navbar-search bg-white'>
             <div className='flex align-center'>
-              <input type = "text" className='form-control fs-14' placeholder='Search your preferred items here' onChange={(e) => handleSearchTerm(e)} />
-              <Link to = {`search/${searchTerm}`} className='text-white search-btn flex align-center justify-center'>
-                  <i className='fa-solid fa-magnifying-glass'></i>
-                </Link>
+              <input
+                type="text"
+                className='form-control fs-14'
+                placeholder='Search your preferred items here'
+                onChange={handleSearchTerm}
+              />
+              <Link to={`search/${searchTerm}`} className='text-white search-btn flex align-center justify-center'>
+                <i className='fa-solid fa-magnifying-glass'></i>
+              </Link>
             </div>
           </div>
 
-<ul className='navbar-nav flex align-center fs-12 fw-4 font-manrope'>
-  {
-    Array.isArray(categories) && categories.length > 0 &&
-    categories.slice(0, 8).map((category, idx) => (
-      category && category.slug && category.name && (
-        <li className='nav-item no-wrap' key={idx}>
-          <Link to={`/category/${category.slug}`} className='nav-link text-capitalize'>
-            {category.name}
-          </Link>
-        </li>
-      )
-    ))
-  }
-</ul>
-
+          <ul className='navbar-nav flex align-center fs-12 fw-4 font-manrope'>
+            {
+              Array.isArray(categories) && categories.length > 0 &&
+              categories.slice(0, 8).map((category, idx) => (
+                category && category.slug && category.name && (
+                  <li className='nav-item no-wrap' key={idx}>
+                    <Link to={`/category/${category.slug}`} className='nav-link text-capitalize'>
+                      {category.name}
+                    </Link>
+                  </li>
+                )
+              ))
+            }
+          </ul>
         </div>
 
         <div className='navbar-cart flex align-center'>
-          <Link to = "/cart" className='cart-btn'>
+          <Link to="/cart" className='cart-btn'>
             <i className='fa-solid fa-cart-shopping'></i>
             <div className='cart-items-value'>{itemsCount}</div>
-            <CartModal carts = {carts} />
+            <CartModal carts={carts} />
           </Link>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
