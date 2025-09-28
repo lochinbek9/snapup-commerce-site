5fa38d6eedeb5a500546c16658b0fec4dd06cff0
Lochinbek Dehkonov <103257271+lochinbek9@users.noreply.github.com>
2025-09-28 09:30:35 +0500

    Update categorySlice.js

--- src/store/categorySlice.js
+++ src/store/categorySlice.js
@@ -1,13 +1,29 @@
-import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {BASE_URL} from "../utils/apiURL";
-import {STATUS} from "../utils/status";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { BASE_URL } from "../utils/apiURL";
+import { STATUS } from "../utils/status";
+
+export const fetchAsyncCategories = createAsyncThunk('categories/fetch', async () => {
+    const response = await fetch(`${BASE_URL}products/categories`);
+    const data = await response.json();
+    return data.map((cat) => ({
+        name: cat.replace(/-/g, ' '), // "home-decoration" => "home decoration"
+        slug: cat,
+        url: `${BASE_URL}products/category/${cat}`
+    }));
+});
+
+export const fetchAsyncProductsOfCategory = createAsyncThunk('category-products/fetch', async (category) => {
+    const response = await fetch(`${BASE_URL}products/category/${category}`);
+    const data = await response.json();
+    return data.products;
+});
 
 const initialState = {
     categories: [],
     categoriesStatus: STATUS.IDLE,
     categoryProducts: [],
     categoryProductsStatus: STATUS.IDLE
-}
+};
 
 const categorySlice = createSlice({
     name: 'category',
@@ -15,44 +31,28 @@ const categorySlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-        .addCase(fetchAsyncCategories.pending, (state, action) => {
-            state.categoriesStatus = STATUS.LOADING;
-        })
+            .addCase(fetchAsyncCategories.pending, (state) => {
+                state.categoriesStatus = STATUS.LOADING;
+            })
+            .addCase(fetchAsyncCategories.fulfilled, (state, action) => {
+                state.categories = action.payload;
+                state.categoriesStatus = STATUS.SUCCEEDED;
+            })
+            .addCase(fetchAsyncCategories.rejected, (state) => {
+                state.categoriesStatus = STATUS.FAILED;
+            })
 
-        .addCase(fetchAsyncCategories.fulfilled, (state, action) => {
-            state.categories = action.payload;
-            state.categoriesStatus = STATUS.SUCCEEDED;
-        })
-
-        .addCase(fetchAsyncCategories.rejected, (state, action) => {
-            state.categoriesStatus = STATUS.FAILED;
-        })
-
-        .addCase(fetchAsyncProductsOfCategory.pending, (state, action) => {
-            state.categoryProductsStatus = STATUS.LOADING;
-        })
-
-        .addCase(fetchAsyncProductsOfCategory.fulfilled, (state, action) => {
-            state.categoryProducts = action.payload;
-            state.categoryProductsStatus = STATUS.SUCCEEDED;
-        })
-
-        .addCase(fetchAsyncProductsOfCategory.rejected, (state, action) => {
-            state.categoryProductsStatus = STATUS.FAILED;
-        })
+            .addCase(fetchAsyncProductsOfCategory.pending, (state) => {
+                state.categoryProductsStatus = STATUS.LOADING;
+            })
+            .addCase(fetchAsyncProductsOfCategory.fulfilled, (state, action) => {
+                state.categoryProducts = action.payload;
+                state.categoryProductsStatus = STATUS.SUCCEEDED;
+            })
+            .addCase(fetchAsyncProductsOfCategory.rejected, (state) => {
+                state.categoryProductsStatus = STATUS.FAILED;
+            });
     }
-});
-
-export const fetchAsyncCategories = createAsyncThunk('categories/fetch', async() => {
-    const response = await fetch(`${BASE_URL}products/categories`);
-    const data = await response.json();
-    return data;
-});
-
-export const fetchAsyncProductsOfCategory = createAsyncThunk('category-products/fetch', async(category) => {
-    const response = await fetch(`${BASE_URL}products/category/${category}`);
-    const data = await response.json();
-    return data.products;
 });
 
 export const getAllCategories = (state) => state.category.categories;
